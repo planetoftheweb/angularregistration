e2aa30114e5cb7984288ac39766701751e31982d
Ray Villalobos <ray@planetoftheweb.com>
2015-09-27 15:01:36 -0400

    added files

--- builds/angularregistration/js/controllers/success.js
+++
@@ -1,7 +0,0 @@
-myApp.controller('SuccessController', ['$scope', '$rootScope',
- 'FIREBASE_URL',
-  function($scope, $rootScope, FIREBASE_URL) {
-
-    $scope.message = 'Welcome to my App';
-
-}]); //MeetingsController
