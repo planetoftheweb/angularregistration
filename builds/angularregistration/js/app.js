396d4e31973803a6aa9d32ddb815e6383debd196
Ray Villalobos <ray@lynda.com>
2015-09-28 15:32:17 -0400

    01_03e Using Routes

--- builds/angularregistration/js/app.js
+++ builds/angularregistration/js/app.js
@@ -1,5 +1,20 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('appController', ['$scope', function($scope) {
-  $scope.message = "Welcome to my App";
-}])
+myApp.config(['$routeProvider'], function($routeProvider) {
+  $routeProvider.
+    when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'RegistrationController'
+    }).
+    when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'RegistrationController'
+    }).
+    when('/success', {
+      templateUrl: 'views/success.html',
+      controller: 'SuccessController'
+    }).
+    otherwise({
+      redirectTo: '/login'
+    });
+});
