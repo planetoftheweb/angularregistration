394610cf02da67513de97cd5253cb8be4b446fd3
Ray Villalobos <ray@lynda.com>
2015-09-29 18:26:07 -0400

    01_06e Managing submissions

--- builds/angularregistration/js/controllers/registration.js
+++ builds/angularregistration/js/controllers/registration.js
@@ -1,3 +1,11 @@
 myApp.controller('RegistrationController', ['$scope', function($scope) {
-  $scope.message = "Welcome to my App";
+  
+  $scope.login = function() {
+    $scope.message = "Welcome " + $scope.user.email;
+  };
+
+  $scope.register = function() {
+    $scope.message = "Welcome " + $scope.user.firstname;
+  };
+
 }]);
