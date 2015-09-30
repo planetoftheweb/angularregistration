9b4b27465dc2b937280f10b3b8849c77d118c8b1
Ray Villalobos <ray@lynda.com>
2015-09-30 09:32:22 -0400

    02_05e Loggin Users In

--- builds/angularregistration/js/services/authentication.js
+++ builds/angularregistration/js/services/authentication.js
@@ -1,12 +1,20 @@
-myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', 'FIREBASE_URL',
-  function($rootScope, $firebaseAuth, FIREBASE_URL) {
+myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$location', 'FIREBASE_URL',
+  function($rootScope, $firebaseAuth,
+    $location, FIREBASE_URL) {
 
   var ref = new Firebase(FIREBASE_URL);
   var auth = $firebaseAuth(ref);
 
   return {
     login: function(user) {
-      $rootScope.message = "Welcome " + $scope.user.email;
+      auth.$authWithPassword({
+        email: user.email,
+        password: user.password
+      }).then(function(regUser) {
+        $location.path('/success');
+      }).catch(function(error) {
+       $rootScope.message = error.message;
+      });
     }, //login
 
     register: function(user) {
