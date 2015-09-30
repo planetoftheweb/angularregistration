58fd2a7fcf7e38c44f8758039ef3e61eb2271204
Ray Villalobos <ray@lynda.com>
2015-09-30 00:42:36 -0400

    changes to update code

--- builds/angularregistration/js/controllers/registration.js
+++ builds/angularregistration/js/controllers/registration.js
@@ -1,24 +1,13 @@
 myApp.controller('RegistrationController',
-  ['$scope', '$firebaseAuth', 'FIREBASE_URL',
-  function($scope, $firebaseAuth, FIREBASE_URL) {
-
-  var ref = new Firebase(FIREBASE_URL);
-  var auth = $firebaseAuth(ref);
+  ['$scope', 'Authentication',
+  function($scope, Authentication) {
   
   $scope.login = function() {
-    $scope.message = "Welcome " + $scope.user.email;
+    Authentication.login($scope.user);
   }; //login
 
   $scope.register = function() {
-    auth.$createUser({
-      email: $scope.user.email,
-      password: $scope.user.password
-    }).then(function(regUser) {
-      $scope.message = "Hi " + $scope.user.firstname +
-      ", Thanks for registering";
-    }).catch(function(error) {
-      $scope.message = error.message;
-    }); // //createUser
+    Authentication.register($scope.user);
   }; // register
 
 }]); // Controller
