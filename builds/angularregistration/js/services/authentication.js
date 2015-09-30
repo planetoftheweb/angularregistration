dd0cf950cb9a6e4af96c7791a440cc22b8431183
Ray Villalobos <ray@lynda.com>
2015-09-30 10:29:53 -0400

    02_06e Registration Events

--- builds/angularregistration/js/services/authentication.js
+++ builds/angularregistration/js/services/authentication.js
@@ -1,9 +1,22 @@
-myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$location', 'FIREBASE_URL',
-  function($rootScope, $firebaseAuth,
+myApp.factory('Authentication', 
+  ['$rootScope', '$firebaseAuth', '$firebaseObject',
+  '$location', 'FIREBASE_URL',
+  function($rootScope, $firebaseAuth, $firebaseObject,
     $location, FIREBASE_URL) {
 
   var ref = new Firebase(FIREBASE_URL);
   var auth = $firebaseAuth(ref);
+
+  auth.$onAuth(function(authUser) {
+    if (authUser) {
+      var userRef = new Firebase(FIREBASE_URL + 'users/' + authUser.uid );
+      var userObj = $firebaseObject(userRef);
+      $rootScope.currentUser = userObj;
+    } else {
+      $rootScope.currentUser = '';
+    }
+  });
+
 
   return {
     login: function(user) {
