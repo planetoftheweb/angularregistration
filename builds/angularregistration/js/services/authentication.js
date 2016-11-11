64b6abb8d70c540f1d5539d4b3f6a8b60b90a865
Ray Villalobos <ray@lynda.com>
2016-11-10 19:55:55 -0500

    02_05e login users

--- builds/angularregistration/js/services/authentication.js
+++ builds/angularregistration/js/services/authentication.js
@@ -1,13 +1,20 @@
 myApp.factory('Authentication',
-  ['$rootScope', '$firebaseAuth',
-  function($rootScope, $firebaseAuth) {
+  ['$rootScope', '$location', '$firebaseAuth',
+  function($rootScope, $location, $firebaseAuth) {
 
   var ref = firebase.database().ref();
   var auth = $firebaseAuth();
 
   return {
     login: function(user) {
-      $rootScope.message = "Welcome " + $rootScope.user.email;
+      auth.$signInWithEmailAndPassword(
+        user.email,
+        user.password
+      ).then(function(user) {
+        $location.path('/success');
+      }).catch(function(error) {
+        $rootScope.message = error.message;
+      }) //signInWithEmailAndPassword
     }, //login
 
     register: function(user) {
